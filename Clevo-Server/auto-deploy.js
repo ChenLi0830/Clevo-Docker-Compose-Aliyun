1d25992c19fc4cbf226c890173d46baef4d340aa
Chen Li <yichen.li0830@gmail.com>
2017-11-12 17:10:58 -0800

    Use npm-bundle js command instead of bash command

--- Clevo-Server/auto-deploy.js
+++ Clevo-Server/auto-deploy.js
@@ -1,5 +1,6 @@
-const child_process = require('child_process')
+const childProcess = require('child_process')
 const ossClient = require('../ossClient')
+var npmBundle = require('npm-bundle')
 
 console.log(`Stage:${process.env.STAGE} - Clevo-Server`)
 console.log('auto-deploy start...')
@@ -7,8 +8,18 @@ console.log('auto-deploy start...')
 // Create tarball
 console.log('------ Creating tarball file ------')
 let fileName = 'clevo-server-1.0.0.tgz'
-child_process.execSync(`npm-bundle ${fileName}`)
-console.log('------ Success ------')
+// child_process.execSync(`npm-bundle ${fileName}`)
+var args = []
+var options = {
+  verbose: true
+}
+npmBundle(args, options, function onNpmBundle (error, output) {
+  if (error) {
+    throw error
+  }
+  process.stdout.write(output.file)
+  console.log('------ Success ------')
+})
 
 // Upload tarball to OSS
 console.log('------ Uploading tar file to OSS ------')
@@ -32,7 +43,7 @@ ossClient.put(fileName, filePath)
     // console.log('curl command', `curl --insecure --cert ${certDir}/cert.pem --key ${certDir}/key.pem -X POST ${clusterUrl}/projects/${projectName}/redeploy`)
     try {
         // Todo: use node request / fetch to make the rest call
-      child_process.execSync(`curl --insecure --cert ${certDir}/cert.pem --key ${certDir}/key.pem -X POST ${clusterUrl}/projects/${projectName}/redeploy`)
+      childProcess.execSync(`curl --insecure --cert ${certDir}/cert.pem --key ${certDir}/key.pem -X POST ${clusterUrl}/projects/${projectName}/redeploy`)
       console.log('------ Success ------')
     } catch (err) {
       console.log('Redeploy cluster Error', err)
